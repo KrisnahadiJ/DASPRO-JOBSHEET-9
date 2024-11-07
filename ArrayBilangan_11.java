8efee2415660a90f7e7b5d26abb6c4cc1710a29b
KrisnahadiJ <krisnahadij@gmail.com>
2024-11-07 15:09:57 +0700

    Mengisi Elemen Array

---
+++ ArrayBilangan_11.java
@@ -0,0 +1,9 @@
+public class ArrayBilangan_11 {
+    public static void main(String[] args) {
+        int[] bil = {5, 13, -7, 17}; //isi data array langsung saat dideklarasikan
+
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
+    }
+}
